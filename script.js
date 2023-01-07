7d330d5bf821554ffa6a849e4fbac26aab382357
Ankit Kumar Roy <ankitdasout@gmail.com>
2023-01-07 19:06:49 +0530

    style
    styled

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let isAlive = true
 
-let passwordOneEl = document.querySelector("#password-one")
-let passwordTwoEl = document.querySelector("#password-two")
-let btnEl = document.querySelector("#btn-el")
+const passwordOneEl = document.querySelector("#password-one")
+const passwordTwoEl = document.querySelector("#password-two")
+const btnEl = document.querySelector("#btn-el")
+// const btnOne = document.querySelector("#copy-btn-one")
+// const btnTwo = document.querySelector("#copy-btn-two")
 
 
 btnEl.addEventListener("click", function(){
@@ -14,8 +15,13 @@ btnEl.addEventListener("click", function(){
         passwordOneEl.textContent += characters[passOne]
         passwordTwoEl.textContent += characters[passTwo]
     }
-    isAlive = false
 })
 
+// btnOne.addEventListener("click", function(){
 
-// console.log(btnEl)
+//     navigator.clipboard.writeText(passwordOneEl.value)
+//     console.log(passwordOneEl.value)
+// })
+
+
+console.log(btnTwo)
